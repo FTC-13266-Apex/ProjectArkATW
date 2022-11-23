2a57a729748bc466cacb1c73b48a3d44c33cf094
Justin <you@example.com>
2022-11-23 01:32:16 -0600

    Made Base Subsystem class that can be implemented by subsystems. it allows for locking the tele op functions in case a command would like to do so

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/BaseSubsystem.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public abstract class BaseSubsystem {
+    private boolean isLocked;
+    protected OpMode opMode;
+    public BaseSubsystem(@NonNull OpMode opmode) {
+        this.opMode = opmode;
+    }
+
+    /**
+     * Checks if the subsystem can be controlled with the gamepad
+     * @return Whether the subsystem is locked or not
+     */
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    /**
+     * Prevents subsystem from being controlled by gamepad
+     */
+    public void lock() {
+        isLocked = true;
+    }
+
+    /**
+     * Allows Subsystem to be controlled by gamepad
+     */
+    public void unlock() {
+        isLocked = false;
+    }
+
+    public void controlWithGamepad() {
+        if (isLocked()) return;
+        manualControl();
+    }
+
+    protected abstract void manualControl();
+}
