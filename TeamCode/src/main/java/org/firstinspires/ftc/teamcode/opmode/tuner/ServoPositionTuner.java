69e8f4b13efb85883e7939fe3b8cba979ca7d663
Anti-Shulk <84753002+Anti-Shulk@users.noreply.github.com>
2022-11-23 19:09:38 -0600

    Servo Position Tuner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuner/ServoPositionTuner.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.opmode.tuner;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.command.FlipCone;
+import org.firstinspires.ftc.teamcode.subsystem.ConeFlipper;
+import org.firstinspires.ftc.teamcode.subsystem.Drive;
+import org.firstinspires.ftc.teamcode.subsystem.Gripper;
+import org.firstinspires.ftc.teamcode.subsystem.Lift;
+
+@TeleOp
+public class ServoPositionTuner extends LinearOpMode {
+    public static class Constants {
+        public static String name = "coneFlipperBottom";
+        public static double positionChangeSpeed = 0.0001;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo servo = hardwareMap.get(Servo.class, Constants.name);
+        double position = 0;
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                position += Constants.positionChangeSpeed;
+                servo.setPosition(position);
+            }
+            else if (gamepad1.b) {
+                position -= Constants.positionChangeSpeed;
+                servo.setPosition(position);
+            }
+            telemetry.addData("Tuning Servo", Constants.name);
+            telemetry.addData("Position", position);
+        }
+
+    }
+}
+
