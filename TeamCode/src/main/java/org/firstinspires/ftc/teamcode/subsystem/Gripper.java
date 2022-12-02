cd120ef99a42ef7ec0b4a33f0c7bca1f1d9710e4
Justin <you@example.com>
2022-12-02 00:58:15 -0600

    Sensor control on gripper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Gripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Gripper.java
@@ -3,12 +3,16 @@ package org.firstinspires.ftc.teamcode.subsystem;
 import androidx.annotation.NonNull;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Gripper extends Subsystem {
     public static class Constants {
         public static Hardware hardware;
         public static Position position;
+        public static Sensor sensor;
 
         private static class Hardware {
 
@@ -17,26 +21,34 @@ public class Gripper extends Subsystem {
         }
 
         private static class Position {
-            public static double
-                    OPEN = .51,
-                    CLOSE = .65;
+            public static double OPEN = .51;
+            public static double CLOSE = .65;
+        }
 
-
+        private static class Sensor {
+            public static double GRAB_DISTANCE_MM = 70;
         }
     }
 
     private final Servo gripper;
+    private final ColorRangeSensor distanceSensor;
 
     public Gripper (@NonNull OpMode opMode) {
         super(opMode);
         gripper = opMode.hardwareMap.get(Servo.class, "gripper");
         gripper.setDirection(Constants.Hardware.DIRECTION);
+        distanceSensor = opMode.hardwareMap.get(ColorRangeSensor.class, "distanceSensor");
     }
 
     @Override
     protected void manualControl() {
         if (opMode.gamepad2.a) close();
         else if (opMode.gamepad2.b) open();
+        sensorControl();
+    }
+
+    private void sensorControl() {
+        if (distanceSensor.getDistance(DistanceUnit.MM) < Constants.Sensor.GRAB_DISTANCE_MM) close();
     }
 
     public void open() {
