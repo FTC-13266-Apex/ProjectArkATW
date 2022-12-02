9d9c31bb4ced598fb8434d100c974a58c2a20659
Anti-Shulk <84753002+Anti-Shulk@users.noreply.github.com>
2022-12-02 17:14:36 -0600

    Pose2d Container

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/RightSidePathOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/RightSidePathOnly.java
@@ -10,7 +10,9 @@ import org.firstinspires.ftc.teamcode.subsystem.Drive;
 import org.firstinspires.ftc.teamcode.subsystem.Gripper;
 import org.firstinspires.ftc.teamcode.subsystem.Lift;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.trajectorysequence.container.Pose2dContainer;
 import org.firstinspires.ftc.teamcode.util.PoseStorage;
+import org.firstinspires.ftc.teamcode.trajectorysequence.container.SplineToSplineHeading;
 
 @Autonomous
 public class RightSidePathOnly extends LinearOpMode {
@@ -23,23 +25,20 @@ public class RightSidePathOnly extends LinearOpMode {
 
         public static Path path;
         public static class Path {
-            public static Pose2d startPose = new Pose2d(31, -62, Math.toRadians(90));
+            public static Pose2dContainer startPose = new Pose2dContainer(31, -62, 90);
             public static PreLoad preload;
             public static class PreLoad {
-                public static Pose2d endPose = new Pose2d(32,-6 , Math.toRadians(135));
-                public static double endHeading = Math.toRadians(115);
+                public static SplineToSplineHeading splineToSplineHeading = new SplineToSplineHeading(32, -6, 135, 115);
             }
             public static Cycle1Pickup cycle1Pickup;
             public static class Cycle1Pickup {
-                public static Pose2d endPose = new Pose2d(59, -12, Math.toRadians(0));
-                public static double endHeading = Math.toRadians(135);
+                public static SplineToSplineHeading splineToSplineHeading = new SplineToSplineHeading(59, -12, 0, 135);
                 public static double forwardDistance = 5;
             }
             public static Cycle1Drop cycle1Drop;
             public static class Cycle1Drop {
                 public static double backDistance = 5;
-                public static Pose2d endPose = new Pose2d(32,-6 , Math.toRadians(135));
-                public static double endHeading = Math.toRadians(135);
+                public static SplineToSplineHeading splineToSplineHeading = new SplineToSplineHeading(32, -6, 135, 135);
 
             }
         }
@@ -57,26 +56,29 @@ public class RightSidePathOnly extends LinearOpMode {
 
         gripper.close();
 
-        TrajectorySequence preLoad = drive.trajectorySequenceBuilder(Constants.Path.startPose)
+        Pose2d startPose = new Pose2d(
+                Constants.Path.startPose.x,
+                Constants.Path.startPose.y,
+                Math.toRadians(Constants.Path.startPose.heading));
+
+        TrajectorySequence preLoad = drive.trajectorySequenceBuilder(startPose)
                 .setReversed(false)
-                .splineToSplineHeading(Constants.Path.PreLoad.endPose, Constants.Path.PreLoad.endHeading, vel, accel) // The lower the right number is, the more the bot will go right. Higher = more straight of a path
+                .splineToSplineHeading(Constants.Path.PreLoad.splineToSplineHeading, vel, accel) // The lower the right number is, the more the bot will go right. Higher = more straight of a path
                 .build();
 
         TrajectorySequence cycle1Pickup = drive.trajectorySequenceBuilder(preLoad.end())
                 .setReversed(true)
-                .splineToSplineHeading(Constants.Path.Cycle1Pickup.endPose, Constants.Path.Cycle1Pickup.endHeading, vel, accel)
+                .splineToSplineHeading(Constants.Path.Cycle1Pickup.splineToSplineHeading, vel, accel)
                 .forward(Constants.Path.Cycle1Pickup.forwardDistance, vel, accel)
                 .build();
 
         TrajectorySequence cycle1Drop = drive.trajectorySequenceBuilder(cycle1Pickup.end())
                 .setReversed(false)
                 .back(Constants.Path.Cycle1Drop.backDistance, vel, accel)
-                .splineToSplineHeading(Constants.Path.Cycle1Drop.endPose, Constants.Path.Cycle1Drop.endHeading, vel, accel)
+                .splineToSplineHeading(Constants.Path.Cycle1Drop.splineToSplineHeading, vel, accel)
                 .build();
 
-
-
-        drive.setPoseEstimate(Constants.Path.startPose);
+        drive.setPoseEstimate(startPose);
 
 
         waitForStart();
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/container/Pose2dContainer.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.trajectorysequence.container;
+
+public class Pose2dContainer {
+    public double x = 0;
+    public double y = 0;
+    public double heading = 0;
+    public Pose2dContainer(double x, double y, double heading) {
+        this.x = x;
+        this.y = y;
+        this.heading = heading;
+    }
+}
