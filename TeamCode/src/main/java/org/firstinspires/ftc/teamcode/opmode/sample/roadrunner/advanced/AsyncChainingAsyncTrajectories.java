024b57e374838137e6d8c5b5a74d173792cf595a
Justin <you@example.com>
2022-12-02 00:07:02 -0600

    Updated commend on AsyncChainingAsyncTrajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/sample/roadrunner/advanced/AsyncChainingAsyncTrajectories.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/sample/roadrunner/advanced/AsyncChainingAsyncTrajectories.java
@@ -12,22 +12,9 @@ import org.firstinspires.ftc.teamcode.subsystem.Drive;
 import org.firstinspires.ftc.teamcode.util.PoseStorage;
 
 /**
- * This opmode explains how you follow multiple trajectories in succession, asynchronously. This
- * allows you to run your own logic beside the drive.update() command. This enables one to run
- * their own loops in the background such as a PID controller for a lift. We can also continuously
- * write our pose to PoseStorage.
- * <p>
- * The use of a State enum and a currentState field constitutes a "finite state machine."
- * You should understand the basics of what a state machine is prior to reading this opmode. A good
- * explanation can be found here:
- * https://www.youtube.com/watch?v=Pu7PMN5NGkQ (A finite state machine introduction tailored to FTC)
- * or here:
- * https://gm0.org/en/stable/docs/software/finite-state-machines.html (gm0's article on FSM's)
- * <p>
- * You can expand upon the FSM concept and take advantage of command based programming, subsystems,
- * state charts (for cyclical and strongly enforced states), etc. There is still a lot to do
- * to supercharge your code. This can be much cleaner by abstracting many of these things. This
- * opmode only serves as an initial starting point.
+ * This opmode explains how you follow multiple trajectories in succession, asynchronously but using
+ * inline displacement markers
+ * @see  <a href=https://learnroadrunner.com/advanced.html#chaining-async-trajectories>Learn Road Runner Advanced Chaining Async Trajectories</a>
  */
 @Autonomous(group = "Z advanced")
 public class AsyncChainingAsyncTrajectories extends LinearOpMode {
