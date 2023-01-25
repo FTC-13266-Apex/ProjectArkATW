0e84840df572c5a96a77d2d0f2e7039ac737b4e4
Justin <jschwerdtfeger14@gmail.com>
2023-01-24 22:34:14 -0600

    optimize lynxmodules

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/OptimizeStuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/OptimizeStuff.java
@@ -2,11 +2,16 @@ package org.firstinspires.ftc.teamcode.command;
 
 import androidx.annotation.NonNull;
 
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class OptimizeStuff extends InstantCommand {
     public OptimizeStuff(@NonNull LinearOpMode opMode) {
         super(opMode);
         opMode.telemetry.setMsTransmissionInterval(50);
+
+        for (LynxModule module : opMode.hardwareMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
     }
 }
