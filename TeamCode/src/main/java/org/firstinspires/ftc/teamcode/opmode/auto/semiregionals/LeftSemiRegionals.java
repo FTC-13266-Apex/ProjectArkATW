957877611d33fe190aed5e3cc991b253309a203e
chris <christopher9ram@gmail.com>
2023-02-14 18:33:06 -0600

    scadvfgh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/semiregionals/LeftSemiRegionals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/semiregionals/LeftSemiRegionals.java
@@ -19,7 +19,7 @@ public class LeftSemiRegionals extends RoadRunnerAuto {
     public static class Constants {
         public static Speed speed;
         public static class Speed {
-            public static double baseVel = 35; // value
+            public static double baseVel = 25; // value
             public static double baseAccel = 60; // value
             public static double turnVel = 60; // value
             public static double turnAccel = 60; // value
@@ -27,7 +27,7 @@ public class LeftSemiRegionals extends RoadRunnerAuto {
 
         public static WaitSeconds waitSeconds;
         public static class WaitSeconds {
-            public static double dropWait = 1.5;
+            public static double dropWait = .1;
             public static double pickupLiftWait = 0;
         }
 
@@ -40,25 +40,25 @@ public class LeftSemiRegionals extends RoadRunnerAuto {
             public static double midDistance = 12;
             public static double rightDistance = -12;
 
-            public static Forward preload1 = new Forward(8);
-            public static LineToLinearHeading preload2 = new LineToLinearHeading(-36, -24, 180);
-            static TrajectorySequenceContainer preload = new TrajectorySequenceContainer(preload1, preload2);
+          //  public static Forward preload1 = new Forward(1);
+            public static LineToLinearHeading preload2 = new LineToLinearHeading(-38, -26, 180);
+            static TrajectorySequenceContainer preload = new TrajectorySequenceContainer(preload2);
 
             public static StrafeRight park1 = new StrafeRight(midDistance);
             static TrajectorySequenceContainer park = new TrajectorySequenceContainer(park1);
 
             public static StrafeRight cycle1Pickup1 = new StrafeRight(3);
-            public static SplineToConstantHeading cycle1Pickup2 = new SplineToConstantHeading(-46, -11, 180);
+            public static SplineToConstantHeading cycle1Pickup2 = new SplineToConstantHeading(-51, -16, 180);
             public static Forward cycle1Pickup3 = new Forward(10);
             static TrajectorySequenceContainer cycle1Pickup = new TrajectorySequenceContainer(cycle1Pickup1, cycle1Pickup2, cycle1Pickup3);
 
-            public static Back cycle1Drop1 = new Back(0);
-            public static LineToSplineHeading cycle1Drop2 = new LineToSplineHeading(-24, -11, -90);
+            public static Back cycle1Drop1 = new Back(1);
+            public static LineToSplineHeading cycle1Drop2 = new LineToSplineHeading(-26, -16, -90);
             static TrajectorySequenceContainer cycle1Drop = new TrajectorySequenceContainer(cycle1Drop1, cycle1Drop2);
 
-//            public static SplineToSplineHeading cycle2Pickup1 = new SplineToSplineHeading(57, -11, 0, 0);
-//            public static Forward cycle2Pickup2 = new Forward(7);
-//            static TrajectorySequenceContainer cycle2Pickup = new TrajectorySequenceContainer(cycle2Pickup1, cycle2Pickup2);
+            public static LineToSplineHeading cycle2Pickup1 = new LineToSplineHeading(-51, -16, 180);
+            public static Forward cycle2Pickup2 = new Forward(10);
+            static TrajectorySequenceContainer cycle2Pickup = new TrajectorySequenceContainer(cycle2Pickup1, cycle2Pickup2);
 //
 //            public static Back cycle2Drop1 = new Back(5);
 //            public static SplineToSplineHeading cycle2Drop2 = new SplineToSplineHeading(32, -1, 135, 115);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/StandardTrackingWheelLocalizer.java
@@ -31,7 +31,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         public static double WHEEL_RADIUS = 0.6889764; // in
         public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-        public static double LATERAL_DISTANCE = 11.05; // in; distance between the left and right wheels
+        public static double LATERAL_DISTANCE = 10.5; // in; distance between the left and right wheels
         public static double FORWARD_OFFSET = -5; // in; offset of the lateral wheel
 
         public static double X_MULTIPLIER = 1.015; // Multiplier in the X direction
